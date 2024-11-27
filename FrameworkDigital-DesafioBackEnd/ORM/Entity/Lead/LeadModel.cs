6d5b4c7c7f6c02a171b1f3b7fb7240228f2236e1
Gabriel Santos <gabrielosantosb@gmail.com>
2024-11-27 11:22:50 -0300

    Ajustando Model

--- FrameworkDigital-DesafioBackEnd/ORM/Entity/Lead/LeadModel.cs
+++ FrameworkDigital-DesafioBackEnd/ORM/Entity/Lead/LeadModel.cs
@@ -2,17 +2,17 @@
 {
     public class LeadModel
     {
-
         public int LeadId { get; set; }
         public string ContactFirstName { get; set; }
+        public string ContactLastName { get; set; } 
+        public string ContactEmail { get; set; }    
+        public string ContactPhoneNumber { get; set; } 
         public DateTime DateCreated { get; set; }
         public string Suburb { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public bool IsAccepted { get; set; }
-
-
+        public bool IsInvitedStatus { get; set; } 
 
     }
 }
